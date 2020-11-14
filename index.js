fb7394e878cf4d3fd56d00381e1228219c978a14
Charles Metzger <cmetzjr@yahoo.com>
2020-11-14 09:48:44 -0500

    add role working

--- index.js
+++ index.js
@@ -50,103 +50,170 @@ function showPrompts() {
 
             case "Add Employee":
                 //input new role and show prompts    
-                newEmployeePrompt();
-
-                function newEmployeePrompt() {
-                    inquirer.prompt({
-                        name: "newEmployee",
-                        type: "input",
-                        message: "New employee's name:"
-                    }).then(function (answer) {
-                        var employeeName = answer.newEmployee;
-                        addEmployee(employeeName);
-                        console.log(`\n ${employeeName} added - what would you like to do next? \n`)
-                        showPrompts();
-                    })
-                };
+                newEmpPrompt();
                 break;
 
             case "Exit":
                 exit();
         };
     });
-
-
-
-
-
-    // ===================================================
-    //exit app
-    function exit() {
-        connection.end();
-    }
-
-    //===========================================
-    //view departments
-    function viewDepts() {
-        connection.query("SELECT name FROM employees.department", function (err, res) {
-            if (err) throw err;
-            console.log("\n");
-            console.table(res);
-            showPrompts();
-        })
-    };
-
-    //===========================================
-    //add department
-    function newDeptPrompt() {
-                    inquirer.prompt({
-                        name: "newDept",
-                        type: "input",
-                        message: "New department name:"
-                    }).then(function (answer) {
-                        var depName = answer.newDept;
-                        addDept(depName);
-                        console.log(`${depName} added - what would you like to do next?`)
-                        showPrompts();
-                    })
-                };
-    
-    function addDept(depName) {
-        var query = "INSERT INTO department (name) VALUES (?)";
-        connection.query(query, depName, function (err, res) {
-            if (err) throw err;
-        });
-    };
-
-    //===========================================
-    //view roles
-    function viewRoles() {
-        connection.query("SELECT title FROM employees.role", function (err, res) {
-            if (err) throw err;
-            console.log("\n");
-            console.table(res);
-            showPrompts();
-        })
-    };
-
-    //===========================================
-    //add role
-    function newRolePrompt(roleName) {
-        var query = "INSERT INTO role (title, salary, department_id) VALUES (?)";
-        connection.query(query, roleName, function (err, res) {
-            if (err) throw err;
-        });
-    };
-
-    //===========================================
-    //view employees
-    function viewEmployees() {
-        connection.query("SELECT id, first_name, last_name FROM employees.employee;", function (err, res) {
-            if (err) throw err;
-            console.log("\n");
-            console.table(res);
-            showPrompts();
-        })
-    };
-
-    //===========================================
-    //add employee
-    
-
 }
+
+
+
+
+
+// ===================================================
+//exit app
+function exit() {
+    connection.end();
+}
+
+//===========================================
+//view departments
+function viewDepts() {
+    connection.query("SELECT name FROM employees.department", function (err, res) {
+        if (err) throw err;
+        console.log("\n");
+        console.table(res);
+        showPrompts();
+    })
+};
+
+//===========================================
+//add department
+function newDeptPrompt() {
+    inquirer.prompt({
+        name: "newDept",
+        type: "input",
+        message: "New department name:"
+    }).then(function (answer) {
+        var query = "INSERT INTO department (name) VALUES (?)";
+        connection.query(query, answer.newDept, function (err, res) {
+            if (err) throw err;
+        });
+        console.log(`${answer.newDept} added - what would you like to do next?`)
+        showPrompts();
+
+    })
+};
+
+
+//===========================================
+//view roles
+function viewRoles() {
+    connection.query("SELECT title FROM employees.role", function (err, res) {
+        if (err) throw err;
+        console.log("\n");
+        console.table(res);
+        showPrompts();
+    })
+};
+
+//===========================================
+//add role
+function newRolePrompt() {
+    var deptQuery = 'SELECT * FROM employees.department';
+    connection.query(deptQuery, function (err, res) {
+        var deptChoices = res.map(dep => {
+            return {
+                name: dep.name,
+                value: dep.id
+            }
+        })
+        inquirer.prompt([{
+            name: "newTitle",
+            type: "input",
+            message: "New Role name:"
+        }, {
+            name: "newSalary",
+            type: "input",
+            message: "Salary:"
+        }, {
+            name: "newId",
+            type: "list",
+            message: "Department ID:",
+            choices: deptChoices
+        }]).then(function (answer) {
+            var query = "INSERT INTO role SET ?";
+            connection.query(
+                query, {
+                    "title": answer.newTitle,
+                    "salary": answer.newSalary,
+                    // "department_id": answer.newID
+                    "department_id": 1
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(`New role of ${answer.newTitle} added - what would you like to do next?`)
+                    showPrompts();
+                });
+        })
+    })
+};
+
+
+//===========================================
+//view employees
+function viewEmployees() {
+    connection.query("SELECT id, first_name, last_name FROM employees.employee;", function (err, res) {
+        if (err) throw err;
+        console.log("\n");
+        console.table(res);
+        showPrompts();
+    })
+};
+
+//===========================================
+//add employee
+// function newEmpPrompt() {
+//     var roleQuery = 'SELECT * FROM employees.role';
+//     connection.query(roleQuery, function (err, res) {
+//         var roleChoices = res.map(role => {
+//             return {
+//                 name: role.title,
+//                 value: role.id
+//             }
+//         })
+//         var mgrQuery = 'SELECT first_name, last_name, id FROM employees.employee WHERE role_id=1 OR role_id = 2 OR role_id = 3';
+//         connection.query(mgrQuery, function (err, res) {
+//             var mgrChoices = res.map(mgr => {
+//                 return {
+//                     first_name: mgr.first_name,
+//                     last_name: mgr.last_name,
+//                     value: mgr.id
+//                 }
+//             })
+
+//             inquirer.prompt({
+//                 name: "newEmpFirst",
+//                 type: "input",
+//                 message: "Employee's first name:"
+//             }, {
+//                 name: "newEmpLast",
+//                 type: "input",
+//                 message: "Employee's last name:"
+//             }, {
+//                 name: "newEmpRole",
+//                 type: "list",
+//                 message: "Employee's role:",
+//                 choices: roleChoices
+//             }, {
+//                 name: "newEmpManager",
+//                 type: "list",
+//                 message: "Employee's manager (if avail):",
+//                 choices: mgrChoices
+//             }).then(function (answer) {
+//                 var query = "INSERT INTO employee (name) VALUES (?)";
+//                 connection.query(query, answer.newDept, function (err, res) {
+//                     if (err) throw err;
+//                 });
+//                 console.log(`${answer.newDept} added - what would you like to do next?`)
+//                 showPrompts();
+
+//             })
+//         })
+//     });
+
+
+// }
